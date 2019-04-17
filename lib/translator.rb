e09eab0e6a5f554acab485c9eb5199c9c2de55f6
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-17 17:25:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,8 @@ def load_library(file_path)
   YAML.load_file(file_path).each do |meaning, emoticon|
     library["get_emoticon"][english] = japanese
     library["get_meaning"][japanese] = meaning
-    
-
+  end
+  library
 end
 
 def get_japanese_emoticon
