5842b8447482ca85e9558f75d65b8ee927d4d8cb
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-17 17:23:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require'yaml'
 def load_library(file_path)
   library = {"get_meaning" =>{}, get_emoticon => {}}
   YAML.load_file(file_path).each do |meaning, emoticon|
-    
+    library[get_meaning][:english]
 end
 
 def get_japanese_emoticon
