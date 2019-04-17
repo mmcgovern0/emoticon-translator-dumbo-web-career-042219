4629ecf7a9028f5a67a7f9b294a2c5809d0be669
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-17 17:29:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,8 +21,9 @@ def get_japanese_emoticon(file_path, emoticon)
   end
 end
 
-def get_english_meaning
-  library = load_library[file_path]
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  
 end
 
 
