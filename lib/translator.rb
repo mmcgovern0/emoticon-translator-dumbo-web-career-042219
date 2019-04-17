e7951eecd795fa55b3486f31eaa365cc1c72ad5b
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-17 17:28:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,8 @@ def get_japanese_emoticon(file_path, emoticon)
   if result
     result
   else
-    "Sorry that emoticon was not found"
-  # code goes here
+    "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning
