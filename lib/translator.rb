b4d31cdbfbd5f29729540cd40e11d7f01c35cd83
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-17 17:30:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,10 @@ end
 def get_english_meaning(file_path, emoticon)
   library = load_library(file_path)
   result = library["get_meaning"][emoticon]
-  
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
 end
 
 
